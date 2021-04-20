53a6cb22543bffefcc19e219b7c3ede737529856
The_philosopher <shinudonney@tuta.io>
2021-04-20 17:26:16 +0530

    Improve utils map

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,53 +1,42 @@
 --|  @keymappings.lua  |--
 
-
 local utils = require('utils')
 
 -- leader key
 vim.g.mapleader = ' '
 utils.map('n', '<Space>', '')
 
-
 -- toggle higlight
 utils.map('n', '<Leader>h', ':set hlsearch!<CR>')
-
 
 -- easy escape
 utils.map('i', 'jk', '<ESC>')
 utils.map('i', 'kj', '<ESC>')
 utils.map('i', 'jj', '<ESC>')
 
-
 -- explorer
 utils.map('n', '<Leader>e', ':NvimTreeToggle<CR>')
-
 
 -- indenting
 utils.map('v', '<', '<gv')
 utils.map('v', '>', '>gv')
 
-
 -- switch buffer
 utils.map('n', '<TAB>', ':bnext<CR>')
 utils.map('n', '<S-TAB>', ':bprevious<CR>')
-
 
 -- move lines in visual mode
 utils.map('x', 'K', [[:move '<-2<CR>gv-gv']])
 utils.map('x', 'J', [[:move '>+1<CR>gv-gv']])
 
-
 -- completion
-utils.map('i', '<expr><TAB>', [[pumvisible() ? '\<C-n>' : '\<TAB>']])
-utils.map('i', '<expr><S-TAB>', [[pumvisible() ? '\<C-p>' : '\<S-TAB>']])
-
-
-
-local opt = { silent = true }
+local opt = { expr = true }
+utils.map('i', '<expr><TAB>', [[pumvisible() ? '\<C-n>' : '\<TAB>']], opt)
+utils.map('i', '<expr><S-TAB>', [[pumvisible() ? '\<C-p>' : '\<S-TAB>']], opt)
 
 -- better window navigation
+local opt = { noremap = false }
 utils.map('n', '<C-h>', '<C-w>h', opt)
 utils.map('n', '<C-j>', '<C-w>j', opt)
 utils.map('n', '<C-k>', '<C-w>k', opt)
 utils.map('n', '<C-l>', '<C-w>l', opt)
-
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,9 +1,8 @@
 local utils = {}
 
-function utils.map (mode, lhs, rhs, options)
-  if(options == nil) then
-    options = { noremap = true, silent = true }
-  end
+function utils.map (mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
