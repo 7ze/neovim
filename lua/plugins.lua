8f4ee6136b52ff1551963b6d6bda0e50810c5d11
The_philosopher <shinudonney@tuta.io>
2021-04-20 17:52:09 +0530

    Refactor plugin manager code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,7 @@
 --| @plugins.lua |--
 
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
-end
+-- check if packer is installed (~/local/share/nvim/site/pack)
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 
 return require('packer').startup( function() 
   use 'wbthomason/packer.nvim'			-- package manager
