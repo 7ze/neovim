56c68f5c6c49205ebbf7d63da859fc9bd258dff2
The_philosopher <shinudonney@tuta.io>
2021-04-23 10:17:28 +0530

    Replace double quotes with single

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,12 +22,12 @@ function(use)
   use 'hrsh7th/vim-vsnip'
 
   -- Telescope
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
-  use "nvim-telescope/telescope.nvim"
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
 
   -- Treesitter
-  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
   -- explorer and icons
   use 'kyazdani42/nvim-tree.lua'
